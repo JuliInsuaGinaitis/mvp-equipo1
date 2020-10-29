d64579083cd0fe16712c217edf4b381f587c51c6
PabloMombiela <pablomombiela@live.com.ar>
2020-10-29 01:56:52 -0300

    Formularios create/edit admin. detalles de css con los partial

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const methodOverride = require('method-override'); //para PUT y DELETE//
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,64 +18,64 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(methodOverride('_method')); //para PUT y DELETE//
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
-  let file = path.resolve("views/index.html");
-  res.sendFile(file);
+app.get("/", function(req, res) {
+    let file = path.resolve("views/index.html");
+    res.sendFile(file);
 });
 
-app.get("/login", function (req, res) {
-  let file = path.resolve("views/login.html");
-  res.sendFile(file);
+app.get("/login", function(req, res) {
+    let file = path.resolve("views/login.html");
+    res.sendFile(file);
 });
 
-app.get("/register", function (req, res) {
-  let file = path.resolve("views/register.html");
-  res.sendFile(file);
+app.get("/register", function(req, res) {
+    let file = path.resolve("views/register.html");
+    res.sendFile(file);
 });
 
-app.get("/productCart", function (req, res) {
-  let file = path.resolve("views/productCart.html");
-  res.sendFile(file);
+app.get("/productCart", function(req, res) {
+    let file = path.resolve("views/productCart.html");
+    res.sendFile(file);
 });
 
-app.get("/productDetail", function (req, res) {
-  let file = path.resolve("views/productDetail.html");
-  res.sendFile(file);
+app.get("/productDetail", function(req, res) {
+    let file = path.resolve("views/productDetail.html");
+    res.sendFile(file);
 });
 
 app.get("*", (req, res) => {
-  if (req.url.endsWith(".css")) {
-    let file = path.resolve("style" + req.url);
-    return res.sendFile(file);
-  }
-  let images = ["jpg", "jpeg", "gif", "png", "svg"];
-  let ext = req.url.split(".")[1];
-  if (images.includes(ext)) {
-    let file = path.resolve("img" + req.url);
-    return res.sendFile(file);
-  }
+    if (req.url.endsWith(".css")) {
+        let file = path.resolve("style" + req.url);
+        return res.sendFile(file);
+    }
+    let images = ["jpg", "jpeg", "gif", "png", "svg"];
+    let ext = req.url.split(".")[1];
+    if (images.includes(ext)) {
+        let file = path.resolve("img" + req.url);
+        return res.sendFile(file);
+    }
 });
 
 
